6c2172366208aad029be3c4a0a9c5e4be03bc2ab
spectraaa <aliyev.iof@gmail.com>
2020-01-11 03:21:30 +0300

    upd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import time
-from multiprocessing.pool import ThreadPool
+from multiprocessing.pool import Pool
+from multiprocessing import Manager
 
 #import seaborn as pd
 
@@ -15,17 +16,20 @@ cluster_df.to_csv('out.csv')
 transactions = cluster_df.transaction_id.unique()
 start = time.time()
 
-first_to_second = 0
-second_to_first = 0
+manager = Manager()
 
-first_to_zero = 0
-zero_to_first = 0
 
-second_to_zero = 0
-zero_to_second = 0
+first_to_second = manager.list()
+second_to_first = manager.list()
 
-first_fee = 0
-second_fee = 0
+first_to_zero = manager.list()
+zero_to_first = manager.list()
+
+second_to_zero = manager.list()
+zero_to_second = manager.list()
+
+first_fee = manager.list()
+second_fee = manager.list()
 
 def check_tr(i):
     global first_to_second,second_to_first,first_to_zero,zero_to_first,second_to_zero,zero_to_second,first_fee,second_fee
@@ -47,10 +51,10 @@ def check_tr(i):
             second_received = second['received'].sum()
 
             if first_sent != 0:
-                first_to_second += second_received
+                first_to_second.append(second_received)
 
             if second_sent != 0:
-                second_to_first += first_received
+                second_to_first.append(first_received)
 
         if ((any(transaction_df.cluster_id == 1)) and (any(transaction_df.cluster_id == 0))):
             if first is None:
@@ -65,10 +69,10 @@ def check_tr(i):
             zero_received = zero['received'].sum()
 
             if first_sent != 0:
-                first_to_zero += zero_received
+                first_to_zero.append(zero_received)
 
             if zero_sent != 0:
-                zero_to_first += first_received
+                zero_to_first.append(first_received)
 
         if ((any(transaction_df.cluster_id == 2)) and (any(transaction_df.cluster_id == 0))):
             if second is None:
@@ -82,10 +86,10 @@ def check_tr(i):
             zero_received = zero['received'].sum()
 
             if second_sent != 0:
-                second_to_zero += zero_received
+                second_to_zero.append(zero_received)
 
             if zero_sent != 0:
-                zero_to_second += second_received
+                zero_to_second.append(second_received)
 
         if ((any(transaction_df.cluster_id == 1)) or (any(transaction_df.cluster_id == 2))):
             if first is None:
@@ -97,25 +101,24 @@ def check_tr(i):
             second_sent = second['sent'].sum()
 
             if first_sent != 0:
-                first_fee += first_sent - transaction_df['received'].sum()
-
+                first_fee.append(first_sent - transaction_df['received'].sum())
             if second_sent != 0:
-                second_fee += second_sent - transaction_df['received'].sum()
+                second_fee.append(second_sent - transaction_df['received'].sum())
 
-pool = ThreadPool(1)
+pool = Pool(4)
 #
 for i in pool.imap_unordered(check_tr, range(0,len(transactions) - 1)):
     pass
 print('Done',time.time() - start)
 
-print('From the first to the second cluster', first_to_second/100000000)
-print('From the second to the first cluster', second_to_first/100000000)
+print('From the first to the second cluster', sum(first_to_second)/100000000)
+print('From the second to the first cluster', sum(second_to_first)/100000000)
 
-print('From the first to the zero cluster', zero_to_first/100000000)
-print('From the zero to the first cluster', first_to_zero/100000000)
+print('From the first to the zero cluster', sum(zero_to_first)/100000000)
+print('From the zero to the first cluster', sum(first_to_zero)/100000000)
 
-print('From the second to the zero cluster', second_to_zero/100000000)
-print('From the zero to the second cluster', zero_to_second/100000000)
+print('From the second to the zero cluster', sum(second_to_zero)/100000000)
+print('From the zero to the second cluster', sum(zero_to_second)/100000000)
 
-print('First cluster fee', first_fee/100000000)
-print('Second cluster fee', second_fee/100000000)
+print('First cluster fee', sum(first_fee)/100000000)
+print('Second cluster fee', sum(second_fee)/100000000)
